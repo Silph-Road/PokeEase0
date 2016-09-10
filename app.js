333fbcab99edd0d0ce67343c56c7876174a5256f
Truong Nguyen <samuraitruong@hotmail.com>
2016-09-10 22:29:41 +1000

    try to fix case sensitive asset url for heroku

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var port = process.env.PORT || 3000;
 var app = express();
+app.set('case sensitive routing','false')
 app.use(express.static(__dirname + '/public'))
 console.log('server running at http://localhost:3000 .......')
 app.listen(port);
